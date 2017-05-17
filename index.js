8636a8877b7302297ab401bfa078d625ddc22599
rayGreycloud <rayGreycloud@gmail.com>
2017-05-17 06:55:54 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// My version
+module.exports = name => `Hello ${name}!`;
+
+// Andrew's ES6 version
+// module.exports = (name) => {
+//   return `Hello ${name}!`;
+// };
+// Old JS
+// module.exports = function (name) {
+//   return "Hello " + name + "!";
+// };
